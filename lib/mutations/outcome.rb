262f89a00304c371d442b833649d82872619b0e9
holgersindbaek <holgersindbaek@gmail.com>
2016-09-14 14:04:47 -0400

    Hotfix

--- lib/mutations/outcome.rb
+++ lib/mutations/outcome.rb
@@ -23,14 +23,9 @@ module Mutations
     end
 
     def response
-      ap "response 1"
       if self.success?
-        ap "response 2"
-        ap ({ json: self.json, status: self.result[:status] || 200 })
         { json: self.json, status: self.result[:status] || 200 }
       else
-        ap "response 3"
-        ap ({ json: ({ errors: @errors.symbolic }).merge(self.result), status: self.result[:status] || 400 })
         { json: ({ errors: @errors.symbolic }).merge(self.result), status: self.result[:status] || 400 }
       end
     end
